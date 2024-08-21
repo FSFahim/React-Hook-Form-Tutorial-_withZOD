1acb98b0d5f623f7b013ce1a6aff0c4d1ffe4189
FSFahim <farhanshahriar3728@gmail.com>
2024-08-21 22:32:48 +0600

    Form control and validation using React-hook-form

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import HookFormUsage from './components/2-hook-form'
+
+function App() {
+
+  return (
+    <div>
+      <h1>React Hook Form Tutorial</h1>
+      <HookFormUsage/>
+    </div>
+  )
+}
+
+export default App
